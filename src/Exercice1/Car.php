3c58b50bb24f2a55f6e472932930bf16ba7fef66
Pulsy BACKEKOLO <pulsycaleb@gmail.com>
2025-01-07 12:07:51 +0100

    exo1

--- src/Exercice1/Car.php
+++ src/Exercice1/Car.php
@@ -2,6 +2,49 @@
 
 namespace EdemotsCourses\EsgiDesignPattern\Exercice1;
 
-class Car
+interface Vehicule {
+    public function accelerate(): float;
+    public function break(): float;
+}
+
+class Car implements Vehicule
 {
+    private float $speed = 0;
+
+    public function accelerate(): float
+    {
+        $this->speed += 3.5;
+        return $this->speed;
+    }
+
+    public function break(): float
+    {
+        $this->speed -= 5;
+        if($this->speed <= 0 ){
+            return 0;
+        }
+        
+        return $this->speed;
+    }
+}
+
+class Truck implements Vehicule
+{
+    private float $speed = 0;
+
+    public function accelerate(): float
+    {
+        $this->speed += 1.75;
+        return $this->speed;
+    }
+
+    public function break(): float
+    {
+        $this->speed -= 2;
+        if($this->speed <= 0 ){
+            return 0;
+        }
+        
+        return $this->speed;
+    }
 }
