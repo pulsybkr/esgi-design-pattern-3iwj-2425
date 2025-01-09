43af5fa69f080f9916928d4547d6876f3d42bf1f
Eliot Demots <eliot@demots.fr>
2025-01-09 01:24:59 +0100

    :sparkles: exercice 8

---
+++ src/Exercice8/TemperatureSensor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EdemotsCourses\EsgiDesignPattern\Exercice8;
+
+class TemperatureSensor
+{
+    public function getCurrentTemperature(): float
+    {
+        return 20.0;
+    }
+}
